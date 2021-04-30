72d5168e484acbb6d1ed8e91c5b437fc5bfd35ca
Sandeep Wadhwa <sandy.wadhwa@gmail.com>
2021-04-30 10:20:33 +0530

    Adding CSS/JS and html code

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express');
  
 const app = express();
 
+// Add a static handler for all static files - CSS/JS/IMG
+app.use(express.static(__dirname+"/frontend"));
+// app.use(express.static(__dirname+"/morestatic"));
+
 // This is home handler
 app.get("/", function(req, res){
     res.send("Welcome to My Basic Site");
 })
  
-
+app.get("/resume", function(req, res){
+    let fullFilePath = __dirname + "/frontend/html/resume.html";
+    res.sendFile(fullFilePath);
+});
 
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
